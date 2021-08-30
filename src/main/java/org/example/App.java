fb15ddd9f8adcb90e6e2b7c35f8db69ee2612d64
gcourtemanche <guilaume.courtemanche@claurendeau.qc.ca>
2021-08-30 15:53:54 -0400

    commit d'initialisation du projet

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
